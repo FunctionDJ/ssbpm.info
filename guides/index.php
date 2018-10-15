1d5db44c5e93d4a21a7d855e322ba6784ef29605
waffeln <roman.assmann.p@gmail.com>
2018-10-15 22:15:56 +0200

    general update

--- guides/index.php
+++ guides/index.php
@@ -17,7 +17,7 @@ $body =  <<<HTML
     Finally this method only allows Brawl mods up to 2GB in size since Brawl itself doesn't read any bigger SD Cards.<br/>
     This method uses the stage loader exploit which is why this limitation exists only for this method and not for the others.</p>
 
-    <div class="w-100 text-center"><a class="btn btn-primary my-2" href="#">Hackless Guide</a></div>
+    <div class="w-100 text-center"><a class="btn btn-primary my-2" href="$lang/guides/hackless/">Hackless Guide</a></div>
     
     <p><span class="h5">Homebrew</span> <span>NTSC</span></p>
     <p>This method requires the Homebrew Channel, a NTSC Wii and a NTSC Super Smash Bros. Brawl physical disc.<br/>
@@ -25,13 +25,13 @@ $body =  <<<HTML
     A furhter advantage over the hackless method is that you can use mods that are more than 2GB in size like Legacy XP and such, and you don't need to remove all stage builder stages.<br/>
     This method launches the Project M (or whatever mod) launcher from the Homebrew Channel to directly inject the files and changes while launching Brawl, so you instantly start the mod.<br/>
 
-    <div class="w-100 text-center"><a class="btn btn-primary my-2" href="#">Homebrew Guide</a></div>
+    <div class="w-100 text-center"><a class="btn btn-primary my-2" href="$lang/guides/homebrew/">Homebrew Guide</a></div>
 
     <p><span class="h5">USB Loading</span> <span>All regions</span></p>
     <p>This is currently the only method for all PAL Wiis or people without a physical working disc of NTSC Super Smash Bros. Brawl.<br/>
     It requires the Homebrew Channel, d2x-cIOS for being able to load Wii Backups (you can load most Wii Games except for Project M and alike from an SD as well, which is why it's Backup Loading and not necessarily USB Loading), a Backup Loader app (USB Loader GX, Configurable USB Loader / CFG Loader, WiiFlow ...) and a NTSC Brawl ISO file.<br/>
 
-    <div class="w-100 text-center"><a class="btn btn-primary my-2" href="#">USB Loading Guide</a></div>
+    <div class="w-100 text-center"><a class="btn btn-primary my-2" href="$lang/guides/usbloading/">USB Loading Guide</a></div>
     
   </div>
 
