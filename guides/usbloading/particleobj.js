0c1570ee0c846e4f68e953f9e4e81e148172a671
waffeln <sinusstudios@gmail.com>
2018-11-15 20:35:42 +0100

    pug transformation

---
+++ guides/usbloading/particleobj.js
@@ -0,0 +1,18 @@
+obj = {
+  id: 'updating',
+  short: 'up',
+  prev: '',
+  next: '',
+  title: 'Updating your Wii',
+  text: 'Go into the Wii settings and update your Wii. You need an internet connection for this. Check if your Wii is version 4.3 after this.',
+  choices: [
+    {
+      target: 'find-wii-version',
+      text: 'My Wii is now updated to 4.3'
+    },
+    {
+      target: 'disc-updating',
+      text: `I can't connect my Wii to the internet or i have a different error.`
+    }
+  ]
+}
