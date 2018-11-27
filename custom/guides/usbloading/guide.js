009d354640b1619f28c2f64a6af620d37c7df56c
waffeln <roman.assmann.p@gmail.com>
2018-11-27 14:57:42 +0100

    replaced the old guidejs with the new one

--- custom/guides/usbloading/guide.js
+++ custom/guides/usbloading/guide.js
@@ -1,161 +1,171 @@
-console.log("guide.js loaded");
-const debugmode = true;
+console.log(`guide.js v2 loaded`);
 
-var currentPage = '';
-var nextChoicePage= '';
+var currentCard = {
+  id: "start"
+};
 
-function debugLog(ind, log) {
-  if (debugmode) {
-    var spaces = '';
-    for (let i = 0; i < ind; i++) {
-      spaces += '  ';
-    }
-    log ? console.log(spaces + log) : console.log('');
+// next button object
+const nextPageButton = {
+  nextPage: '',
+  variable: "heyaaa",
+  button: $('#nextPageButton'),
+  setNext: function(nextPage) {
+    this.nextPage = nextPage;
+    this.button.attr("href", `#${nextPage}`);
+    this.enable();
+  },
+  enable: function() {
+    this.button.removeClass("disabled");
+  },
+  disable: function() {
+    this.button.addClass("disabled");
+  },
+  load: function() {
+    prevPageButton.addPrev(currentCard.id);
+    cards[this.nextPage].show();
+    this.disable();
   }
 }
 
-function nextPage() {
-  debugLog();
-  debugLog(1, 'nextPage() called');
-
-  // get next page
-  let nextPage = nextChoicePage || $(currentPage).data('next');
-  debugLog(2, `Next page is set to: '${nextPage}'`)
-
-  // set prev page attribute
-  $(nextPage).data('prev', currentPage);
-
-  // load next page
-  loadPage(nextPage);
+// prev button object
+const prevPageButton = {
+  queue: [],
+  button: $('#prevPageButton'),
+  enable: function() {
+    this.button.removeClass("disabled");
+  },
+  disable: function() {
+    this.button.addClass("disabled");
+  },
+  addPrev: function(prevPage) {
+    this.queue.push(prevPage);
+    this.enable();
+  },
+  load: function() {
+    let prevPage = this.queue.pop();
+    cards[prevPage].show();
+    this.disable();
+  }
 }
 
-function prevPage() {
-  debugLog();
-  debugLog(1, 'prevPage() called');
+// card class
+class card {
+  constructor(id, short, next, title, text, choices) {
+    this.id = id;
+    this.short = short;
+    this.next = next;
+    this.title = title;
+    this.text = text;
+    this.choices = choices;
+  }
 
-  // get previous page, either from the prev attribute or the fallback
-  // fallback is an attribute pages will have that don't have one exact
-  // previous page, because of user input choices.
-  // the user should land at that choice again.
-  // this is ONLY the case when there has not been made a choice,
-  // for instance, when the user refreshes.
-  // because of the nextPage() function, usually a previous page should be set
-  let prevPage = $(currentPage).data('prev') || $(currentPage).data('fallback');
-
-  // load previous page
-  loadPage(prevPage);
-}
-
-function loadPage(page) {
-  debugLog();
-  debugLog(1, `loadPage('${page}') called`);
-
-  // set current page
-  currentPage = page;
-
-  // set previous button to previous page
-  // get the prevpage attribute from the current page
-  let prevPage = $(currentPage).data('prev');
-  setPrevPage(prevPage);
-
-  // remove all click handlers from all radio buttons
-  $('.radio-input input').off('click');
-  debugLog(2, 'Removed all click handlers from radio buttons')
-
-  // hide all pages
-  $('#pages-container div').hide('fast');
-  debugLog(2, 'Hiding all pages');
-
-  // show new page
-  $(page).show('fast');
-  debugLog(2, `Showing requested page '${page}'`);
-
-  // load next page link
-
-  // if there are any user choices in the new page
-  if ($(`${page} .radio-input`).length > 0) {
-    debugLog(3, 'There are radio buttons on this page');
-
-    $('#nextPageButton').show('fast');
-
-    // if any of the buttons on this page are checked
-    if ($(`${page} .radio-input input`).is(':checked')) {
-      // enable the next page button
-      $('#nextPageButton').removeClass('disabled');
-      debugLog(3, 'Enabled Next button');
-    } else {
-      // disable the next page button
-      $('#nextPageButton').addClass('disabled');
-      debugLog(3, 'Disabled Next button');
-    }
-
-    // if any radio button group (that is the bordered clickable area) is clicked
-    debugLog(3, 'Registering radio button click handlers');
-    $(`${page} .radio-input input`).click(() => {
-      debugLog(1, '');
-      debugLog(1, 'Radio button clicked');
-      
-      // if any of the buttons on this page are checked
-      if ($(`${page} .radio-input input`).is(':checked')) {
-        debugLog(2, 'A radio button is checked');
-        let buttonValue = $(`${page} .radio-input input:checked`).val();
-        debugLog(2, `Value: '${buttonValue}'`);
-
-        // change the next page attribute
-        // this gets the value of the currently selected user option
-        $(page).data('next', buttonValue);
-
-        // set next page attributes on the button
-        setNextPage(buttonValue);
-
-        debugLog(2, `Next attribute now: '${$(page).data('next')}'`)
-
-        // remove the disabled group on the next button (makes it clickable)
-        $('#nextPageButton').removeClass('disabled');
-        debugLog(2, 'Enabled Next button');
+  get HTMLobj() {
+    let required = ["id", "short", "title", "text"];
+    for(let i = 0; i < required.length; i++) {
+      if (this[required[i]] == null) {
+        throw `Error: this.${required[i]} undefined or null.`;
       }
+    }
+
+    if (this.next == null && this.choices == null) {
+      throw `Error: Both this.next and this.choices undefined or null.`;
+    }
+
+    let base = document.createElement("div");
+    base.setAttribute("id", "start");
+    base.setAttribute("style", "display: none");
+
+    let title = document.createElement("h5");
+    title.setAttribute("class", "card-title");
+    let titleText = document.createTextNode(this.title);
+    title.appendChild(titleText);
+    base.appendChild(title);
+
+    let text = document.createElement("p");
+    text.setAttribute("class", "card-text");
+    let textText = document.createTextNode(this.text);
+    text.appendChild(textText);
+    base.appendChild(text);
+
+    if (this.choices) {
+      for (let i = 0; i < this.choices.length; i++) {
+        let label = document.createElement("label");
+        label.setAttribute("for", `${this.short}-${i}`);
+        label.setAttribute("class", "radio-input");
+
+        let input = document.createElement("input");
+        input.setAttribute("type", "radio");
+        input.setAttribute("id", `${this.short}-${i}`);
+        input.setAttribute("value", `${this.choices[i].target}`);
+        input.setAttribute("name", `${this.id}`);
+        label.appendChild(input);
+
+        let labelText = document.createTextNode(this.choices[i].text);
+        label.appendChild(labelText);
+
+        base.appendChild(label);
+
+        let lineBreak = document.createElement("br");
+        base.appendChild(lineBreak);
+      }
+    }
+
+    return base;
+  }
+
+  show() {
+    // for prevPageButton
+    currentCard = this;
+
+    // remove all click handlers
+    $('.radio-input input').off('click');
+
+    let pagesContainer = $('#pages-container');
+
+    // render HTML from object
+    let html = this.HTMLobj;
+
+    // get all children of the pagesContainer
+    // should only be one child but this clears everything
+    let currentPage = $('#pages-container > *');
+
+    // hide every child using jQuery fade out
+    currentPage.hide('fast', () => {
+      pagesContainer.empty();
+      pagesContainer.html(html);
+
+      let newPage = $('#pages-container > *');
+
+      newPage.show('fast', () => {
+        if (this.next) {
+          nextPageButton.setNext(this.next);
+        }
+
+        // if there are still entries in the queue for previous pages
+        if (prevPageButton.queue.length) prevPageButton.enable();
+
+        // add click handlers for new choices even if they don't exist
+        $(`#pages-container > * > .radio-input input`).click(() => {
+          // if any of the buttons on this page are checked
+          if ($(`#pages-container > * > .radio-input input`).is(':checked')) {
+            let buttonValue = $(`#pages-container > * > .radio-input input:checked`).val();
+            nextPageButton.setNext(buttonValue);
+          }
+        });
+      });
     });
-  } else {
-    // there are no radio buttons on this loaded page
-    // set the nextPageButton's values
-    // from the next attribute of the current page
-    let nextPage = $(currentPage).data('next');
-    setNextPage(nextPage);
   }
 }
 
-function setNextPage(page) {
-  debugLog(0, `• setNextPage('${page}') called`);
-  let button = $('#nextPageButton')
-  if (page) {
-    button.attr('href', page);
-    button.show('fast');
-  } else {
-    button.hide();
-  }
-}
+(() => { // run after full page load
+  console.log(`Current window hash: ${window.location.hash}`);
 
-function setPrevPage(page) {
-  debugLog(0, `• setPrevPage('${page}') called`);
-  let button = $('#prevPageButton');
-  if (page) {
-    button.attr('href', page);
-    button.show('fast');
-  } else {
-    button.hide();
-  }
-}
-
-(function() { // run after full page load
-  
-  // if element with the id from the hash in the URI exists
-  if ($(window.location.hash).length) {
-
-    // show it
-    loadPage(window.location.hash);
-  } else {
-
-    // show the first page (not final lol)
-    loadPage('#start');
-  }
+  // initial choice click event handler
+  $(`#pages-container > * > .radio-input input`).click(() => {
+    // if any of the buttons on this page are checked
+    if ($(`#pages-container > * > .radio-input input`).is(':checked')) {
+      let buttonValue = $(`#pages-container > * > .radio-input input:checked`).val();
+      nextPageButton.setNext(buttonValue);
+    }
+  });
 })();
