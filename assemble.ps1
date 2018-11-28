249c0967058ae7e110af0c4d48544d340dabce66
waffeln <roman.assmann.p@gmail.com>
2018-11-28 11:50:51 +0100

    script now also renders main SCSS

--- assemble.ps1
+++ assemble.ps1
@@ -1,4 +1,8 @@
+Write-Host 'Converting SCSS to CSS...'
+sass .\custom\modules\main.scss .\custom\modules\main.css
+
 Write-Host 'Copying customs...'
 xcopy .\custom .\html /EXCLUDE:excludedFilesFromCustom.txt /S /I /R /Y /Q
+
 Write-Host 'Rendering Pug files...'
 node app.js
