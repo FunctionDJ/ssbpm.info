ed4536325bba75607e0b88da0e6bff607c392f96
waffeln <roman.assmann.p@gmail.com>
2018-12-03 13:38:39 +0100

    Copy over showdown.js file on run task

--- assemble.ps1
+++ assemble.ps1
@@ -1,3 +1,6 @@
+Write-Host 'Copying custom showdown.js file...'
+cp .\node_modules\custom-showdown\dist\showdown.min.js .\custom\modules\custom-showdown.js -Force
+
 Write-Host 'Converting SCSS to CSS...'
 sass .\custom\modules\main.scss .\custom\modules\main.css
 
