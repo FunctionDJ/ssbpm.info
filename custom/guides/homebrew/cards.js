6f10bd93982492f34d64a970294fa2085d54a24d
waffeln <sinusstudios@gmail.com>
2018-12-22 00:02:55 +0100

    completely fixed the cards.js for homebrew, now super simple

--- custom/guides/homebrew/cards.js
+++ custom/guides/homebrew/cards.js
@@ -1,43 +1,9 @@
-var cards = {};
-let n = '';
+newCard("start", "st", null, ["bomb", "sd-check", "str2hax"]);
+newCard("bomb", "bm", null, ["letterbomb-1", "bannerbomb", "???"]);
+newCard("letterbomb-1", "lb1", "letterbomb-2");
+newCard("letterbomb-2", "lb2", null, ["letterbomb-3", "wiibrand"]);
+newCard("wiibrand", "wb", "letterbomb-3");
+newCard("str2hax", "s2h");
 
-const getChoices = (n, ...targets) => {
-  let choices = [];
-
-  for(let i = 0; i < targets.length; i++) {
-    choices.push({
-      target: targets[i],
-      text: c[n][0][i]
-    });
-  }
-
-  return choices;
-}
-
-const newCard2 = (id, short, next, choices) => {
-  cards[id] = new card2(id, short, next, choices);
-}
-
-newCard("start", "st", null, ["ntsc-u", "usb-loading", "idk"]);
-
-newCard("ntsc-u", "nu", null, ["ntsc-u-disc", "usb-loading"])
-newCard("usb-loading", "pkj");
-newCard2("idk", "idk", null, ["ntsc-u", "pal-k-j"]);
-
-newCard2("ntsc-u-disc", "nu-d", null, ["ss-hackless", "lb-bb-hackless"]);
-
-// end of card generation
-
-let cardAmount = 0;
-
-for (var key in cards) {
-  cardAmount++;
-}
-
-console.log(`card.js loaded with ${cardAmount} cards.`);
-
-// attempt to load card that's in the window hash
-if (cards[getAnchorCard()]) {
-  console.log(`Card '${getAnchorCard()} loaded since it was found in the hash`)
-  cards[getAnchorCard()].show();
-}
+// required to load the card from anchor on load and logging the card count
+cardsCallback();
